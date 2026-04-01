c3ad8ce22f416af889a69bb3a9c4a6193a39c1a6
Ricardo Alexander <ricardoalexanderh@gmail.com>
2026-04-01 17:20:05 +0700

    Remove Auris product and Play icon from portfolio (#18)
    * Comment out Professional Overview card temporarily
    
    * Temporarily hide Auris from landing page
    
    Comment out Auris links in desktop and mobile nav dropdowns,
    the Auris product showcase card, and the /products/auris route.
    
    * Remove unused Play icon import
    
    The Play icon was only used in the now-commented-out Professional
    Overview card, causing the Vercel build to fail with TS6133.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import RicardoPortfolio from './ricardo-portfolio.tsx'
-import AurisLanding from './auris-landing.tsx'
+// import AurisLanding from './auris-landing.tsx'
 import NowLandingFrontend from './now-landing-frontend.tsx'
 
 createRoot(document.getElementById('root')!).render(
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RicardoPortfolio />} />
-        <Route path="/products/auris" element={<AurisLanding />} />
+        {/* <Route path="/products/auris" element={<AurisLanding />} /> */}
         <Route path="/products/now" element={<NowLandingFrontend />} />
       </Routes>
     </BrowserRouter>
